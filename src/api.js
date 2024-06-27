0b366539b759e38ad4b015bdbf99928a2071b3c9
austinbra <129190342+austinbra@users.noreply.github.com>
2024-06-26 22:29:45 -0500

    initial commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+import { LANGUAGE_VERSIONS } from "./constants";
+
+//const API = axios.create({
+//  baseURL: "https://emkc.org/api/v2/piston",
+//});
+// just anexample using piston api for now btw
+
+export const executeCode = async (language, sourceCode) => {
+  
+
+    //where to pass in the language and the source code assuming the api ur using takes these arguments
+    
+  return response.data;
+};
---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const LANGUAGE_VERSIONS = {
+  python: "3.10.0",
+  java: "15.0.2",
+  csharp: "10.0.0",
+  javascript: "18.15.0",
+  };
+  
+  export const CODE_SNIPPETS = {
+    python: `def say(name):\n\tprint("hola, " + name + "!")\n\nsay("Austin")\n`,
+    java: `public class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Ni Hao World");\n\t}\n}\n`,
+    csharp:'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Ni Hao World in C#");\n\t\t}\n\t}\n}\n',
+    javascript: `function say(name) {\n\tconsole.log("Sup, " + name + "!");\n}\n\nsay("sAustin");\n`,
+  };
