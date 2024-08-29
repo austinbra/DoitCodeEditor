38b9d1c19f93eee06cbedec7e65db67cdd5bcfed
Austin Braugh <129190342+austinbra@users.noreply.github.com>
2024-08-28 21:09:17 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const LANGUAGES = {
   javascript: "18.15.0",
   };
   
-  export const CODE_SNIPPETS = {
+  export const CODE_STARTER = {
     python: `def say(name):\n\tprint("hola, " + name + "!")\n\nsay("Austin")\n`,
     java: `public class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Ni Hao World");\n\t}\n}\n`,
     csharp:'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Ni Hao World in C#");\n\t\t}\n\t}\n}\n',
