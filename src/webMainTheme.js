b2ac14e231c5500170110b00417cee65c3283b77
Austin Braugh <129190342+austinbra@users.noreply.github.com>
2024-08-28 21:29:12 -0500

    Update and rename theme.js to webMainTheme.js

--- src/webMainTheme.js
+++ src/webMainTheme.js
@@ -2,4 +2,4 @@ import {extendTheme} from "@chakra-ui/react";
 const theme = extendTheme({
   config: {initialColorMode: "dark",useSystemColorMode: false,},
 });
-export default theme;
+export default webMainTheme;
