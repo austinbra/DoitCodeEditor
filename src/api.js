f2877b0310a5249c6b7dedac39a65edbf00f50d5
Austin Braugh <129190342+austinbra@users.noreply.github.com>
2024-08-28 21:04:13 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,15 +1,14 @@
 import axios from "axios";
-import { LANGUAGE_VERSIONS } from "./constants";
+import {LANGUAGES} from "./constants";
 
-const API = axios.create({
-  baseURL: "https://emkc.org/api/v2/piston",
-});
+const API = axios.create(
+  {baseURL: "https://emkc.org/api/v2/piston",});
 // just anexample using piston api for now btw
 
-export const executeCode = async (language, sourceCode) => {
+export const runCode = async (language, sourceCode) => {
   const response = await API.post("/execute", {
     language: language,
-    version: LANGUAGE_VERSIONS[language],
+    version: LANGUAGES[language],
     files: [
       {
         content: sourceCode,
