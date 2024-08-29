512624bd7bef2ba3dcab4e18e6ada392baed0207
Austin Braugh <129190342+austinbra@users.noreply.github.com>
2024-08-28 21:12:31 -0500

    Update Output.jsx

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,22 +1,21 @@
-import { useState } from "react";
-import { Box, Button, Text, useToast } from "@chakra-ui/react";
-import { executeCode } from "../api";
+import {useState} from "react";
+import {Box, Button, Text, useToast} from "@chakra-ui/react";
+import {sendCode} from "../api";
 
-const Output = ({ editorRef, language }) => {
+const Output = ({editorRef, language}) => {
   const toast = useToast();
   const [output, setOutput] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-
   const runCode = async () => {
     const sourceCode = editorRef.current.getValue();
     if (!sourceCode) return;
     try {
       setIsLoading(true);
-      const { run: result } = await executeCode(language, sourceCode);
+      const {run: result} = await sendCode(language, sourceCode);
       setOutput(result.output.split("\n"));
       result.stderr ? setIsError(true) : setIsError(false);
-    } catch (error) {
+    } catch(error) {
       console.log(error);
       toast({
         title: "An error occurred.",
@@ -24,9 +23,7 @@ const Output = ({ editorRef, language }) => {
         status: "error",
         duration: 6000,
       });
-    } finally {
-      setIsLoading(false);
-    }
+    }finally{setIsLoading(false);}
   };
 
   return (
