1211de601005b3025a559f35840f11ef2b262914
Austin Braugh <129190342+austinbra@users.noreply.github.com>
2024-08-28 21:29:52 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import theme from "./theme.js";
+import theme from "./webMainTheme.js";
 import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
